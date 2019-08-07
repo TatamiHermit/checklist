5f6b4388cd67be213ad2891ca53c34bb7e99eae2
TatamiHermit <547571190@qq.com>
2019-08-07 22:48:38 +0800

    add teardown()

--- checklist.py
+++ checklist.py
@@ -18,7 +18,7 @@ def get_xlsx():
     return a
 
 
-# 数字转字符串
+# 数字转字母
 def get_char(number):
     factor, moder = divmod(number, 26)
     modchar = chr(moder + 65)
@@ -48,10 +48,15 @@ def write_log(wbname):
 
 def init():
     global app
-    app=xw.App(visible=False,add_book=False)
+    app=xw.App(visible=True,add_book=False)
     app.display_alerts=False
     app.screen_updating=False
 
+
+def teardown():
+    app=xw.App(visible=True)
+    app.display_alerts=True
+    app.screen_updating=True
 
 # read data
 def read_data(bookname):
